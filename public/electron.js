87006c82c6ea57f2e5c99509f7f7b903083b484d
Demi <demi.van.kesteren@gmail.com>
2020-07-25 20:57:41 +0200

    Initial commit

---
+++ public/electron.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+
+let window = null;
+
+function createWindow() {
+  window = new BrowserWindow({width: 1600, height: 680, webPreferences: {nodeIntegration: true}});
+  window.loadURL(`file://${path.join(__dirname, "../public/index.html")}`
+  );
+}
+
+function openMainWindow() {
+  if (window === null) {
+    createWindow();
+  }
+}
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (window === null) {
+    openMainWindow();
+  }
+});
